84a575be0676ce58164baebfdce07c17fdda04b3
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-02-07 19:23:59 +0900

    Use snake_case

--- Web/Groonga/Server.hs
+++ Web/Groonga/Server.hs
@@ -18,23 +18,23 @@ db dbpath = do
 app :: Ptr C'_grn_ctx -> String -> ScottyM ()
 app ctx dbpath = do
     get "/version" $ do
-      ver <- getGroongaVersion
+      ver <- get_groonga_version
       text $ mconcat ["{\"Groonga Version\": \"", ver, "\"}"]
       setHeader "Content-Type" "application/json; charset=utf-8"
     get "/d/:command" $ do
       command <- param "command"
-      response <- sendGroongaCommand ctx (L.unpack command)
+      response <- send_groonga_command ctx (L.unpack command)
       text (L.pack response) -- just to send response. Don't decode with Aeson!
       setHeader "Content-Type" "application/json; charset=utf-8"
 
     where
-      getGroongaVersion :: ActionM L.Text
-      getGroongaVersion = liftIO $ do
+      get_groonga_version :: ActionM L.Text
+      get_groonga_version = liftIO $ do
         version <- Groonga.grn_get_version
         return (L.pack version)
 
-      sendGroongaCommand :: Ptr C'_grn_ctx -> String -> ActionM String
-      sendGroongaCommand ctx command = liftIO $ do
+      send_groonga_command :: Ptr C'_grn_ctx -> String -> ActionM String
+      send_groonga_command ctx command = liftIO $ do
         _ <- Groonga.grn_database_open ctx dbpath
         response <- Groonga.grn_execute_command ctx command
         return response
