d13a0108d5e5afdbc36edd939da29909b06de95c
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-02-07 16:39:12 +0900

    Import

---
+++ Web/Groonga/Server.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Web.Groonga.Server where
+
+import Bindings.Groonga.CommandAPI
+import Web.Scotty
+import Data.Monoid (mconcat)
+import Bindings.Groonga.Raw (C'_grn_ctx)
+import qualified Bindings.Groonga.CommandAPI as Groonga
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
+import Control.Monad.IO.Class
+import Foreign.Ptr
+
+db :: String -> IO (Ptr C'_grn_ctx)
+db dbpath = do
+  ctx <- Groonga.grn_ctx_init
+  Groonga.grn_database_create ctx dbpath
+  return ctx
+
+app :: Ptr C'_grn_ctx -> String -> ScottyM ()
+app ctx dbpath = do
+    get "/d/version" $ do
+      ver <- getGroongaVersion
+      html $ mconcat ["<h1>Groonga Version: ", ver]
+    get "/d/:command" $ do
+      command <- param "command"
+      sendGroongaCommand ctx (L.unpack command)
+      html $ mconcat ["<h1>sent command: ", command, "</h1>"]
+    where
+      getGroongaVersion :: ActionM L.Text
+      getGroongaVersion = liftIO $ do
+        version <- Groonga.grn_get_version
+        return (L.pack version)
+
+      sendGroongaCommand :: Ptr C'_grn_ctx -> String -> ActionM ()
+      sendGroongaCommand ctx command = liftIO $ do
+        _ <- Groonga.grn_database_open ctx dbpath
+        _ <- Groonga.grn_execute_command ctx command
+        return ()
